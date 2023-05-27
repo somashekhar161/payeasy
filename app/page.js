d8c47cfe232b05d223c64bc6357786e3b2fd4c49
Somashekar B R <sanjayrao161@gmail.com>
2023-05-28 01:50:23 +0530

    payeasy-website

--- app/page.js
+++ app/page.js
@@ -1,113 +1,181 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+    <>
+      <nav>
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          className="logo"
+          src="/payeasylogo.png"
+          alt="logo"
+          height="100"
+          width="100"
         />
-      </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Our Features</li>
+          <li>Reviews</li>
+          <li className="download-btn">Download App</li>
+        </ul>
+      </nav>
+      <main>
+        <section className="hero">
+          <div>
+            <h1>All your credit cards, in one place.</h1>
+            <p>join 9M+ members who win rewards and cashbacks everyday</p>
+            <button className="download-btn">Download payeasy</button>
+          </div>
+          <div className="circleParent">
+            <div className="circle"></div>
+          </div>
+        </section>
+        <section className="section2">
+          <h1>
+            We want to make credit card payment, as easy as a few simple taps.
+          </h1>
+          <div className="process">
+            <Image
+              className="logo"
+              src="/assets/faded_circle.png"
+              alt="logo"
+              height="50"
+              width="50"
+            />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+            <hr />
+            <Image
+              className="logo"
+              src="/assets/lock_closed.png"
+              alt="logo"
+              height="50"
+              width="50"
+            />
+            {/* <Image
+              className="logo"
+              src="/assets/line.png"
+              alt="logo"
+              height="50"
+              width="50"
+            /> */}
+            <hr />
+            <Image
+              className="logo"
+              src="/assets/checkmark-circle.png"
+              alt="logo"
+              height="50"
+              width="50"
+            />
+          </div>
+          <button className="download-btn">Get Started</button>
+        </section>
+        <section>
+          <div className="feature-div">
+            <div className="feature-content">
+              <h1>Kakao Wallet</h1>
+              <p>
+                Use Kakao Wallet, the safe and easy digital wallet offered by
+                KakaoTalk.Easily access certificates to issue documents from
+                major public websites.
+              </p>
+            </div>
+            <div className="feature-visual">
+              <div></div>
+            </div>
+          </div>
+          <div className="feature-div">
+            <div className="feature-content">
+              <h1>Kakao Shopping</h1>
+              <p>
+                Shop today through KakaoTalkQuickly and easily look through
+                products offered on KakaoTalk's fourth tab, Kakao Shopping.
+              </p>
+            </div>
+            <div className="feature-visual">
+              <div></div>
+            </div>
+          </div>
+          <div className="feature-div">
+            <div className="feature-content">
+              <h1>Theme</h1>
+              <p>
+                There are ways to spruce up your KakaoTalk, which you use
+                daily.Include your favorite photo and characters to create a
+                customized theme and share it with friends
+              </p>
+            </div>
+            <div className="feature-visual">
+              <div></div>
+            </div>
+          </div>
+        </section>
+        <section className="section2 section2-2">
+          <h1>
+            Get interesting information, that your bank doesn’t otherwise give.
+          </h1>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          <button className="download-btn">Get Started</button>
+        </section>
+        <section className="about-us">
+          <h1>People, Love Us</h1>
+          <div className="stores">
+            <div className="app-store">
+              <h1 id="rating-value">4.8</h1>
+              <h1>app store</h1>
+              <Image
+                src={"/assets/rating.png"}
+                alt="rating"
+                height={300}
+                width={300}
+              />
+              <button className="download-btn logo-btn">
+                {" "}
+                <Image
+                  src={"/assets/apple.png"}
+                  alt="rating"
+                  height={40}
+                  width={40}
+                />
+                Download the app
+              </button>
+            </div>
+            <div className="app-store">
+              <h1 id="rating-value">4.9</h1>
+              <h1>play store</h1>
+              <Image
+                src={"/assets/rating.png"}
+                alt="rating"
+                height={300}
+                width={300}
+              />
+              <button className="download-btn logo-btn">
+                {" "}
+                <Image
+                  src={"/assets/playstore.png"}
+                  alt="rating"
+                  height={40}
+                  width={40}
+                />
+                Download the app
+              </button>
+            </div>
+          </div>
+          <div className="Testamonials">
+            <p>
+              "i would say one of the best platforms to manage your credit card
+              payments with lot of advantages and rewards. you can even pay rent
+              to your owner using credit card. that's really cool. thanks for
+              all the awesomeness. keep inventing !!"
+            </p>
+            <p>-sanjeev kataria</p>
+            <Image src={"/assets/switch-tiles.png"} height={100} width={100} />
+          </div>
+        </section>
+      </main>
+      <footer>
+        <h1>Made with ❤️ by bundlebuild_</h1>
+      </footer>
+    </>
+  );
 }
