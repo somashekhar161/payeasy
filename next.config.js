3e6817603fdbe902ad017d9d94ae49bdc7dd0c17
Somashekar B R <sanjayrao161@gmail.com>
2023-10-04 18:59:40 +0530

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "export" };
+const nextConfig = {
+  output: "export",
+  images: {
+    loader: "custom",
+    loaderFile: "./my-loader.js",
+  },
+};
 
 module.exports = nextConfig;
